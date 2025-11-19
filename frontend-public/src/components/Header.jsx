552de87e1cbac4292eb3f4a572364789dd3305d1
tserentokhtoherkembayr-ops <tserentokhtoherkembayr@gmail.com>
2025-11-19 16:41:15 +0800

    WEBSITE

--- frontend-public/src/components/Header.jsx
+++ frontend-public/src/components/Header.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 
 export default function Header() {
   return (
     <header className="bg-gradient-to-r from-green-200 via-green-400 to-green-600 text-white shadow-md py-4">
       <div className="container mx-auto flex justify-between items-center px-6">
         <h1 className="text-2xl font-bold">🌿 AmraltinGazar</h1>
-        <nav className="space-x-6">
-        </nav>
       </div>
     </header>
   );
