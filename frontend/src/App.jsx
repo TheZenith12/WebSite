98f465ec0316e4f35a76e2829d9afab4a236e118
TheZenith12 <zenitht35@gmail.com>
2025-11-22 19:21:59 +0800

    ss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Sidebar from './components/Sidebar'
-import Topbar from './components/Topbar'
+import AdminLayout from './components/AdminLayout'
 import RequireAuth from './components/RequireAuth'
 import Login from './pages/Login'
 import Resorts from './pages/Resorts'
