1dc7249438429489479c3f61e5760c2fac8b1f8c
TheZenith12 <zenitht35@gmail.com>
2025-11-24 14:18:18 +0800

    ff

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,40 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
-      manifest: {
-        name: "Resorts Admin",
-        short_name: "Resorts",
-        description: "Mongolia Resorts Admin Dashboard",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "/pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
-});
+  base: "/",
+  plugins: [react()],
+  server: {
+    port: 5173
+  }
+})
