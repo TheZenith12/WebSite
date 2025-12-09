2ccb1c5a07b64448a16a74b05b03b242e25d7232
TheZenith12 <zenitht35@gmail.com>
2025-12-09 17:34:37 +0800

    Update resortModel.js

--- backend/src/models/resortModel.js
+++ backend/src/models/resortModel.js
@@ -5,16 +5,31 @@ const resortSchema = new mongoose.Schema(
     name: { type: String, required: true },
     description: String,
     location: String,
+
     lat: {
-       type: Number,
-       required: true
-      },
+      type: Number,
+      required: true,
+    },
+
     lng: {
-       type: Number,
-       required: true
-     },
+      type: Number,
+      required: true,
+    },
+
     price: Number,
-},
+
+    // 🖼️ Зурагнууд
+    images: {
+      type: [String], // Cloudinary URL array
+      default: [],
+    },
+
+    // 🎥 Видео
+    videos: {
+      type: [String], // Cloudinary video URL array
+      default: [],
+    },
+  },
   { timestamps: true }
 );
 
