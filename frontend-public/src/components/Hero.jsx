289fa3f1d74138da1f15d10effc2dac467c7f2cf
TheZenith12 <zenitht35@gmail.com>
2025-12-14 16:29:15 +0800

    Update Hero.jsx

--- frontend-public/src/components/Hero.jsx
+++ frontend-public/src/components/Hero.jsx
@@ -12,6 +12,8 @@ function Hero({ searchTerm, setSearchTerm }) {
           <p className="text-xl text-gray-700 mb-8">
             Байгаль, тайван амралт, тохилог байр таныг хүлээж байна
           </p>
+          
+         
           <div className="max-w-2xl mx-auto">
             <div className="relative">
               <input
