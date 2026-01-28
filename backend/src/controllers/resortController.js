a56051ced35ff3313c7d7ed7a7eb4220b4b7760c
TheZenith12 <zenitht35@gmail.com>
2026-01-28 11:37:42 +0800

    adad

--- backend/src/controllers/resortController.js
+++ backend/src/controllers/resortController.js
@@ -131,13 +131,14 @@ export const updateResort = async (req, res) => {
   try {
     const { id } = req.params;
     alert("0");
+    
+const notifier = require("node-notifier");
 
-{show && (
-  <div className="alert">
-    Амжилттай!
-    <button onClick={() => setShow(false)}>OK</button>
-  </div>
-)}
+notifier.notify({
+  title: "Server Alert",
+  message: "Шинэ request орж ирлээ",
+});
+
 
     let {
       name,
