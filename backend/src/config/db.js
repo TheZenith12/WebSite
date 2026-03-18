b67a7b8b4c6fa36b48169034fe39144dfe101722
tserentokhtoherkembayr-ops <tserentokhtoherkembayr@gmail.com>
2026-03-19 02:20:38 +0800

    fghjk

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,4 +1,20 @@
+<<<<<<< Updated upstream
 import mongoose from "mongoose";
+=======
+
+import mongoose from "mongoose";
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/amraltiin_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+module.exports = mongoose;
+
+
+>>>>>>> Stashed changes
 
 const connectDB = async () => {
   try {
